8943ab94db0ead6187b24cb22a725a474ae82537
Brenner Spear <blspear@gmail.com>
2021-12-01 16:13:04 -0600

    microlink lets gooooooooo

---
+++ pages/api/v1/dev/microlink.ts
@@ -0,0 +1,20 @@
+import mql from '@microlink/mql';
+import fs from 'fs';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import fetch from 'node-fetch';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    console.log('hello');
+    const url = `https://dev.tokengarden.art/viewer`;
+
+    const { status, data, response } = await mql(url, {
+        screenshot: true,
+    });
+    console.log(data);
+
+    const imgdataResponse = await fetch((data as any).screenshot.url);
+    const imgdata = await imgdataResponse.buffer();
+    fs.writeFileSync('test.png', imgdata);
+    res.setHeader('Content-Type', 'image/jpg');
+    res.send(imgdata);
+}
