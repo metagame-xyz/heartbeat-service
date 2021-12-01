da933d9ac29d5544c77fde0e62479caa9a7b77cc
Brenner Spear <blspear@gmail.com>
2021-12-01 12:36:29 -0600

    p5js-node not working, moving to puppeteer

--- types.d.ts
+++ types.d.ts
@@ -1,10 +1,10 @@
-declare module 'p5js-node' {
-    import p5 from 'p5';
+// declare module 'p5js-node' {
+//     import p5 from 'p5';
 
-    class p5jsNode extends p5 {
-        canvas: {
-            toBuffer(): Buffer;
-        };
-    }
-    export default p5jsNode;
-}
+//     class p5jsNode extends p5 {
+//         canvas: {
+//             toBuffer(): Buffer;
+//         };
+//     }
+//     export default p5jsNode;
+// }
