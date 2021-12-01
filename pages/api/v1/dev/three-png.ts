95faacb1d96692f748b7de4214acc201be1fbf20
Brenner Spear <blspear@gmail.com>
2021-11-30 22:35:28 -0600

    try no node-canvas-webgl?

--- pages/api/v1/dev/three-png.ts
+++ pages/api/v1/dev/three-png.ts
@@ -1,6 +1,6 @@
 import { writeFileSync } from 'fs';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { createCanvas } from 'node-canvas-webgl';
+// import { createCanvas } from 'node-canvas-webgl';
 import {
     BoxGeometry,
     Mesh,
@@ -11,32 +11,24 @@ import {
 } from 'three';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const width = 512;
-    const height = 512;
-
-    const scene = new Scene();
-    const camera = new PerspectiveCamera(75, width / height, 0.1, 1000);
-
-    const canvas = createCanvas(width, height);
-
-    const renderer = new WebGLRenderer({
-        canvas,
-    });
-
-    const geometry = new BoxGeometry(1, 1, 1);
-    const material = new MeshBasicMaterial({ color: 'brown' });
-    const cube = new Mesh(geometry, material);
-    scene.add(cube);
-
-    camera.position.z = 5;
-
-    cube.rotation.x += 1;
-    cube.rotation.y += 1;
-
-    renderer.render(scene, camera);
-
-    const img = canvas.toBuffer('image/png');
-    res.setHeader('Content-Type', 'image/png');
-    writeFileSync('threejs-cube.png', img);
-    res.send(img);
+    // const width = 512;
+    // const height = 512;
+    // const scene = new Scene();
+    // const camera = new PerspectiveCamera(75, width / height, 0.1, 1000);
+    // const canvas = createCanvas(width, height);
+    // const renderer = new WebGLRenderer({
+    //     canvas,
+    // });
+    // const geometry = new BoxGeometry(1, 1, 1);
+    // const material = new MeshBasicMaterial({ color: 'brown' });
+    // const cube = new Mesh(geometry, material);
+    // scene.add(cube);
+    // camera.position.z = 5;
+    // cube.rotation.x += 1;
+    // cube.rotation.y += 1;
+    // renderer.render(scene, camera);
+    // const img = canvas.toBuffer('image/png');
+    // res.setHeader('Content-Type', 'image/png');
+    // writeFileSync('threejs-cube.png', img);
+    // res.send(img);
 }
