ed6703ef77b405f867f2744e36874f81cd404971
Brenner Spear <blspear@gmail.com>
2022-01-26 19:11:58 -0600

    change urlbox toekn id

---
+++ pages/api/v1/dev/unsafe/urlbox/[tokenId].ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { generateGIFWithUrlbox } from '@utils/urlbox';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { tokenId } = req.query;
+    const tokenIdString: string = Array.isArray(tokenId) ? tokenId[0] : tokenId;
+
+    const data = await generateGIFWithUrlbox(tokenIdString, true);
+
+    // const openseaMetadata = metadataToOpenSeaMetadata(JSON.parse(metadata));
+    // res.send(data);
+}
