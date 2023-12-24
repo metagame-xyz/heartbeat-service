49ec3c000dca3d5b6d1865d624f68e88cd9303b8
Brenner Spear <blspear@gmail.com>
2023-12-23 18:09:48 -0600

    no more cron

--- pages/api/v1/cronJobs/batchFetchMetadata.ts
+++ pages/api/v1/cronJobs/batchFetchMetadata.ts
@@ -1,48 +1,48 @@
-import { BigNumber, Contract } from 'ethers';
-import { CronJob } from 'quirrel/next';
+// import { BigNumber, Contract } from 'ethers';
+// import { CronJob } from 'quirrel/next';
 
-import updateMetadata from '@api/queues/updateMetadata';
+// import updateMetadata from '@api/queues/updateMetadata';
 
-import { defaultProvider } from '@utils';
-import { CONTRACT_ADDRESS } from '@utils/constants';
-import { LogData, logError, logSuccess } from '@utils/logging';
+// import { defaultProvider } from '@utils';
+// import { CONTRACT_ADDRESS } from '@utils/constants';
+// import { LogData, logError, logSuccess } from '@utils/logging';
 
-import heartbeat from '../../../../heartbeat.json';
+// import heartbeat from '../../../../heartbeat.json';
 
-const jobSpacingInSeconds = 15;
+// const jobSpacingInSeconds = 15;
 
-export default CronJob(
-    'api/v1/cronJobs/batchFetchMetadata', // 👈 the route it's reachable on
-    ['0 3 * * *', 'America/Chicago'], // 👈 the cron schedule
-    async () => {
-        const logData: LogData = {
-            function_name: 'BatchFetchMetadata',
-        };
+// export default CronJob(
+//     'api/v1/cronJobs/batchFetchMetadata', // 👈 the route it's reachable on
+//     ['0 3 * * *', 'America/Chicago'], // 👈 the cron schedule
+//     async () => {
+//         const logData: LogData = {
+//             function_name: 'BatchFetchMetadata',
+//         };
 
-        try {
-            const heartbeatContract = new Contract(
-                CONTRACT_ADDRESS,
-                heartbeat.abi,
-                defaultProvider,
-            );
-            logData.third_party_name = 'ethers';
-            const mintCountBN: BigNumber = await heartbeatContract.mintedCount();
-            const mintCount = mintCountBN.toNumber();
+//         try {
+//             const heartbeatContract = new Contract(
+//                 CONTRACT_ADDRESS,
+//                 heartbeat.abi,
+//                 defaultProvider,
+//             );
+//             logData.third_party_name = 'ethers';
+//             const mintCountBN: BigNumber = await heartbeatContract.mintedCount();
+//             const mintCount = mintCountBN.toNumber();
 
-            // array of tokenIds to update metadata for [1... mintCount]
-            const jobs = [...Array(mintCount + 1).keys()].slice(1).map((id) => {
-                return {
-                    payload: { tokenId: id.toString() },
-                    options: { id: id.toString(), delay: `${id * jobSpacingInSeconds}s` },
-                };
-            });
+//             // array of tokenIds to update metadata for [1... mintCount]
+//             const jobs = [...Array(mintCount + 1).keys()].slice(1).map((id) => {
+//                 return {
+//                     payload: { tokenId: id.toString() },
+//                     options: { id: id.toString(), delay: `${id * jobSpacingInSeconds}s` },
+//                 };
+//             });
 
-            logData.third_party_name = 'quirrel';
-            const jobDataArr = await updateMetadata.enqueueMany(jobs);
-            logData.job_data = jobDataArr[jobDataArr.length - 1];
-            logSuccess(logData, `${jobDataArr.length} jobs enqueued`);
-        } catch (error) {
-            logError(logData, error);
-        }
-    },
-);
+//             logData.third_party_name = 'quirrel';
+//             const jobDataArr = await updateMetadata.enqueueMany(jobs);
+//             logData.job_data = jobDataArr[jobDataArr.length - 1];
+//             logSuccess(logData, `${jobDataArr.length} jobs enqueued`);
+//         } catch (error) {
+//             logError(logData, error);
+//         }
+//     },
+// );
